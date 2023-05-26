a830aa43b2ed1baf71c1228874d61fcf85c1f735
Jakob <jakob.stiel@gmail.com>
2023-05-26 12:09:33 +0200

    Add ArticlePreview

---
+++ components/article/ArticlePreview.vue
@@ -0,0 +1,38 @@
+<template>
+  <ion-item class="article-preview">
+    <ion-label class="preview-label">
+      <p class="tertiary-text">{{ props.date.toLocaleString() }}</p>
+      <h2>{{ props.title }}</h2>
+      <p class="tertiary-text">{{ props.description }}</p>
+    </ion-label>
+  </ion-item>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title: string,
+  description: string,
+  date: Date
+}>();
+</script>
+
+<style scoped lang="scss">
+.article-preview {
+  --padding-start: 0;
+  --padding-end: 0;
+  --padding-top: 0;
+  --padding-bottom: 0;
+  --inner-padding-top: 0;
+  --inner-padding-bottom: 0;
+  --inner-padding-start: 0;
+  --inner-padding-end: 0;
+}
+
+.preview-label {
+  display: flex;
+  flex-direction: column;
+  gap: var(--tiny-spacing);
+  white-space: normal;
+  padding: var(--small-spacing) 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,16 @@
       <div class="container home-page">
         <HomeHeader/>
         <SearchBar/>
-        <h1 class="fancy-text">Popular Articles</h1>
+        <section id="popular-articles">
+          <h1 class="fancy-text">Popular Articles</h1>
+          <ion-list>
+            <ArticlePreview title="Can alcohol be used to treat malaria?
+WHO disagrees with such internet
+claims." description="WHO secretary spoke out against every
+claims concerning such information, but
+some say it’s true." :date="new Date()"/>
+          </ion-list>
+        </section>
       </div>
     </ion-content>
   </ion-page>
@@ -14,10 +23,16 @@
 
 </script>
 
-<style scoped >
+<style scoped>
 .home-page {
   display: flex;
   flex-direction: column;
   gap: var(--large-spacing);
+}
+
+#popular-articles {
+  display: flex;
+  flex-direction: column;
+  gap: var(--small-spacing);
 }
 </style>
