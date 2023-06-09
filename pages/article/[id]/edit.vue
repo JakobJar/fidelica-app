f15f71ac204108d33b5ea84e502b08b87500fa8e
Jakob <jakob.stiel@gmail.com>
2023-06-09 12:01:28 +0200

    Add article page

---
+++ pages/article/[id]/edit.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button defaultHref="/"/>
+        </ion-buttons>
+        <ion-buttons slot="primary">
+          <ion-button>Publish</ion-button>
+        </ion-buttons>
+        <ion-title>Edit Article</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <div class="container">
+        <ion-textarea v-model="content" placeholder="Type the content here..." />
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+const content = useState('content', () => '');
+</script>
+
+<style scoped lang="scss">
+ion-toolbar {
+  --background: var(--background-color);
+  --color: var(--primary-color);
+
+  ion-back-button {
+    --color: var(--primary-color);
+  }
+}
+
+ion-textarea {
+  --color: var(--secondary-color);
+  --highlight-color: var(--secondary-color);
+}
+</style>
---
+++ pages/article/[id]/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <div class="container">
+        <div id="article-content">
+          <h1 v-text="title"/>
+          <p class="secondary-text">Last edited on {{ date.toLocaleString() }} by <span v-text="username" router-link=""/></p>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+const title = useState('title', () => 'Can alcohol be used to treat malaria? WHO disagrees with such internet claims.');
+const description = useState('description', () => 'WHO secretary spoke out against every claims concerning such information, but some say it’s true.');
+const date = useState('date', () => new Date());
+const username = useState('username', () => 'Username');
+
+</script>
+
+<style scoped lang="scss">
+#article-content {
+  display: flex;
+  flex-direction: column;
+  gap: var(--small-spacing);
+}
+</style>
