827e6d265143e9c5e7ab976d52d4900a0bfc8793
Jakob <jakob.stiel@gmail.com>
2023-06-21 11:10:31 +0200

    Add annotation (wip)

---
+++ pages/annotation/url/[url].vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <div class="container">
+        <DefaultHeader default-back-href="/"/>
+        <SearchBar/>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import DefaultHeader from "~/components/header/DefaultHeader.vue";
+
+const runtimeConfig = useRuntimeConfig();
+const route = useRoute();
+const router = useIonRouter();
+
+useSeoMeta({title: "Search | " + runtimeConfig.public.siteName});
+</script>
+
+<style scoped>
+
+</style>
