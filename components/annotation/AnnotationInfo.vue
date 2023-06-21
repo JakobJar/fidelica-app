a3f3e2bfe0de56a3c564647214698c9be1f439f4
Jakob <jakob.stiel@gmail.com>
2023-06-21 22:06:35 +0200

    Annotation progress

--- components/annotation/AnnotationInfo.vue
+++ components/annotation/AnnotationInfo.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="box">
-    <div class="annotation-header">
-      <ion-icon :icon="ioniconsCheckmarkCircle" class="header-icon" />
-      <div>
-        <h1>True</h1>
-        <p class="tertiary-text">This post is considered to be true.</p>
+  <div class="annotation">
+    <div class="annotation-content">
+      <div class="annotation-header">
+        <ion-icon :icon="ioniconsCheckmarkCircle" class="header-icon" />
+        <div>
+          <h1>True</h1>
+          <p class="tertiary-text">This post is considered to be true.</p>
+        </div>
       </div>
-      <div class="votes">
-        <ion-icon :icon="ioniconsArrowUp" class="vote-icon"/>
-        <span>1</span>
-        <ion-icon :icon="ioniconsArrowDown" class="vote-icon"/>
-      </div>
+      <p class="secondary-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia dignissim felis, vel iaculis elit rutrum non. Ut a euismod purus. Donec id mattis tellus. Integer pharetra a nibh in placerat. Integer at vehicula massa. Nulla pulvinar lorem sit amet dolor tempor, et porta enim facilisis. Mauris non venenatis sapien.</p>
     </div>
-    <p class="secondary-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia dignissim felis, vel iaculis elit rutrum non. Ut a euismod purus. Donec id mattis tellus. Integer pharetra a nibh in placerat. Integer at vehicula massa. Nulla pulvinar lorem sit amet dolor tempor, et porta enim facilisis. Mauris non venenatis sapien.</p>
+    <div class="votes">
+      <ion-icon :icon="ioniconsArrowUp" class="vote-icon"/>
+      <span class="vote-count">1</span>
+      <ion-icon :icon="ioniconsArrowDown" class="vote-icon"/>
+    </div>
   </div>
 </template>
 
@@ -21,11 +23,17 @@
 </script>
 
 <style scoped lang="scss">
-.box {
+.annotation {
+  display: flex;
+  align-items: center;
+  gap: var(--small-spacing);
+  padding: var(--small-spacing) 0;
+}
+
+.annotation-content {
   display: flex;
   flex-direction: column;
   gap: var(--small-spacing);
-  padding: var(--small-spacing) 0;
 }
 
 .annotation-header {
@@ -35,19 +43,32 @@
   align-items: center;
 
   .header-icon {
-    font-size: 3rem;
+    font-size: 3.5rem;
     color: var(--primary-color);
   }
+}
 
-  .votes {
-    display: flex;
-    flex-direction: column;
-    gap: var(--tiny-spacing);
-    align-items: center;
-    color: var(--secondary-color);
+.votes {
+  display: flex;
+  flex-direction: column;
+  gap: var(--tiny-spacing);
+  align-items: center;
+  color: var(--secondary-color);
+  margin-left: auto;
 
-    .vote-icon {
-      font-size: 1rem;
+  .vote-count {
+    font-size: 1.25rem;
+  }
+
+  .vote-icon {
+    font-size: 1.5rem;
+
+    &:hover {
+      color: var(--primary-color);
+    }
+
+    &:active {
+      color: var(--highlight-color);
     }
   }
 }
