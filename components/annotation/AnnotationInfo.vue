8ac9361e3318564e9cdeafbe5fc272d4e755e3a1
Jakob <jakob.stiel@gmail.com>
2023-06-21 12:43:36 +0200

    Add annotation (wip)

--- components/annotation/AnnotationInfo.vue
+++ components/annotation/AnnotationInfo.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="box">
-    <h1>True</h1>
-    <p>Lorem ipsum und so</p>
+    <div class="annotation-header">
+      <ion-icon :icon="ioniconsCheckmarkCircle" class="header-icon" />
+      <div>
+        <h1>True</h1>
+        <p class="tertiary-text">This post is considered to be true.</p>
+      </div>
+      <div class="votes">
+        <ion-icon :icon="ioniconsArrowUp" class="vote-icon"/>
+        <span>1</span>
+        <ion-icon :icon="ioniconsArrowDown" class="vote-icon"/>
+      </div>
+    </div>
+    <p class="secondary-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia dignissim felis, vel iaculis elit rutrum non. Ut a euismod purus. Donec id mattis tellus. Integer pharetra a nibh in placerat. Integer at vehicula massa. Nulla pulvinar lorem sit amet dolor tempor, et porta enim facilisis. Mauris non venenatis sapien.</p>
   </div>
 </template>
 
@@ -9,14 +20,35 @@
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .box {
   display: flex;
   flex-direction: column;
   gap: var(--small-spacing);
-  padding: var(--small-spacing);
-  border-radius: var(--normal-border-radius);
-  background: linear-gradient(45deg, var(--tertiary-color), var(--secondary-color));
-  box-shadow: var(--secondary-color) var(--small-spacing);
+  padding: var(--small-spacing) 0;
+}
+
+.annotation-header {
+  display: flex;
+  flex-direction: row;
+  gap: var(--small-spacing);
+  align-items: center;
+
+  .header-icon {
+    font-size: 3rem;
+    color: var(--primary-color);
+  }
+
+  .votes {
+    display: flex;
+    flex-direction: column;
+    gap: var(--tiny-spacing);
+    align-items: center;
+    color: var(--secondary-color);
+
+    .vote-icon {
+      font-size: 1rem;
+    }
+  }
 }
 </style>
