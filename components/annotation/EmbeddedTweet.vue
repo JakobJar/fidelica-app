2aff565a1ad2e2752133e61c6c5e501f58a2fc4a
Jakob <jakob.stiel@gmail.com>
2023-06-22 10:50:10 +0200

    Add EmbeddedTweet (wip)

---
+++ components/annotation/EmbeddedTweet.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="!error && !pending" v-html="data"/>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  url: {
+    type: String,
+    required: true
+  }
+});
+
+const {data, pending, error} = useLazyFetch("https://publish.twitter.com/oembed", {
+  params: {
+    url: props.url,
+    theme: "dark",
+    align: "center",
+    dnt: true,
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
--- pages/annotation/url/[url].vue
+++ pages/annotation/url/[url].vue
@@ -5,6 +5,7 @@
         <DefaultHeader default-back-href="/"/>
         <div class="annotation-page">
           <SearchBar/>
+          <EmbeddedTweet url="https://twitter.com/byVNTY/status/1671655933976498176"/>
           <AnnotationInfo/>
         </div>
       </div>
@@ -14,6 +15,7 @@
 
 <script setup lang="ts">
 import DefaultHeader from "~/components/header/DefaultHeader.vue";
+import EmbeddedTweet from "~/components/annotation/EmbeddedTweet.vue";
 
 const runtimeConfig = useRuntimeConfig();
 const route = useRoute();
